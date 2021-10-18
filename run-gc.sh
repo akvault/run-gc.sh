a1845189a3eb51c1f4303bf56722cbf6708cd3c3
akvault <akndreams@gmail.com>
2021-10-18 21:52:34 +0530

    Update run-gc.sh

--- run-gc.sh
+++ run-gc.sh
@@ -1,1 +1,13 @@
-
+#!/bin/sh
+cd /data/repos
+total=0
+for v in $(ls -d *); do
+  echo $(date) $v
+  cd $v
+  git prune
+  git -c gc.auto=1 -c gc.autoDetach=false gc --auto
+  count=$((count + 1))
+  echo "$count"
+  cd /data/repos
+echo "Total repositories gc: ${total}"
+done
